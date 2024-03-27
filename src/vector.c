16626a4fbc29e4aeac6b310cdb55d60f62952233
Lucas Brioschi <lucaspalaurobrioschi@gmail.com>
2024-03-27 18:25:53 -0300

    Vector pronto, falta testar

--- src/vector.c
+++ src/vector.c
@@ -10,29 +10,52 @@ struct vector{
 
 
 Vector *vector_init(int size, destroy_vector destroy_fn){
-    // TODO
-    return NULL;
+    Vector *v = (Vector *)calloc(1, sizeof(Vector));
+    v->destroy_fn = destroy_fn;
+    v->size = 0;
+    v->capacity = size;
+    v->data = (data_vector *)calloc(v->capacity, sizeof(data_vector));
+    return v;
 }
 
 
-void vector_push_back(data_vector data){
-    //TODO
+void vector_push_back(data_vector data, Vector *v){
+    if(v->size >= v->capacity){
+        v->capacity *= 2;
+        v->data = (data_vector *)realloc(v->data, v->capacity * sizeof(data_vector));
+    }
+    v->data[v->size] = data;
+    v->size++;
 }
 
-data_vector vector_pop_back(){
-    //TODO
-    return NULL;
+data_vector vector_pop_back(Vector *v){
+    if (v->size <= 0)
+    {
+        printf("Empty Vector");
+        exit(1);
+    }
+    data_vector data = v->data[v->size];
+    v->data[v->size] = NULL;
+    v->size--;
+    return data;
 }
 
 int vector_size(Vector *v){
-    // TODO
-    return 0;
+    return v->size;
 }
 
-void vector_get(int idx){
-    //TODO
+data_vector vector_get(int idx, Vector *v){
+    if (idx < 0 || idx >= v->size)
+    {
+        printf("Invalid idx '%d' for Vector with size '%d'.\n", idx, v->size);
+        exit(1);
+    }
+    return v->data[idx];
 }
 
-void vector_destroy(){
-    //TODO
+void vector_destroy(Vector *v){
+    for(int i = 0; i < v->size; i++){
+        v->destroy_fn(v->data[i]);
+    }
+    free(v);
 }
--- src/vector.h
+++ src/vector.h
@@ -11,15 +11,15 @@ Vector *vector_init(int size, destroy_vector destroy_fn);
 int vector_size(Vector *v);
 
 /*Insere na ultima posição do vetor. Se necessário, realoca mais memória*/
-void vector_push_back(data_vector data);
+void vector_push_back(data_vector data, Vector *v);
 
 /*Remove da ultima posição do vetor e retorna o item removido*/
-data_vector vector_pop_back();
+data_vector vector_pop_back(Vector *v);
 
 /*Recupera um item de uma dada posição*/
-void vector_get(int idx);
+data_vector vector_get(int idx, Vector *v);
 
 /*Desaloca o vetor inclusive todos os itens*/
-void vector_destroy();
+void vector_destroy(Vector *v);
 
 #endif 
