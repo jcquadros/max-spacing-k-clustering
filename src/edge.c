0d56f5071a59b3dc28665d420b8fad74ae7142a0
jcquadros <julliepaper@gmail.com>
2024-03-28 09:41:10 -0300

    edge update

--- src/edge.c
+++ src/edge.c
@@ -8,31 +8,35 @@ struct edge{
 }; 
 
 Edge *edge_init(int vertex1, int vertex2, double weight){
-    // TODO
-    return NULL;
+    Edge * ed = (Edge*) malloc(sizeof(Edge));
+    ed->vertex1 = vertex1;
+    ed->vertex2 = vertex2;
+    ed->weight = weight;
+
+    return ed;
 }
 
 int edge_get_vertex1(Edge *ed){
-    // TODO
-    return 0;
+    return ed->vertex1;
 }
 
 int edge_get_vertex2(Edge *ed){
-    // TODO
-    return 0;
+    return ed->vertex2;
 }
 
 double edge_get_weight(Edge *ed){
-    // TODO
-    return 0.0;
+    return ed->weight;
 }
 
-
 void edge_destroy(void *ed){
-    // TODO
+    free((Edge*)ed);
 }
 
 int edge_compare(Edge *ed1, Edge *ed2){
-    // TODO
+    if(ed1->weight > ed2->weight){
+        return 1;
+    }else if(ed1->weight < ed2->weight){
+        return -1;
+    }
     return 0;
 }
